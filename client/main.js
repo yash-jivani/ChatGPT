e6b72eac388a037ecbafa09919a9be275333d0cb
yash jivani <yashjivani17@gmail.com>
2023-01-17 22:54:19 +0530

    ChatGPT v1.1

--- client/main.js
+++ client/main.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fatch data from the server (bot's response)
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://chatgpt-yash.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
